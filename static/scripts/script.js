82db6193314edd60d6cc3dc40385510386a53020
= <=>
2022-10-22 17:33:05 +0600

    Fixed some menu problems for smaller devices

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -2,3 +2,8 @@ function menuClick() {
     document.getElementById('menu').classList.toggle('fa-times');
     document.getElementById('header').classList.toggle('toggle');
 }
+
+window.addEventListener('scroll', function () {
+    document.getElementById('menu').classList.remove('fa-times');
+    document.getElementById('header').classList.remove('toggle');
+})
